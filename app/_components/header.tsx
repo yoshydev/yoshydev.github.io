548b38dbb8347066c31ee4687c2da29aefb456c2
yoshy <24353896+yoshydev@users.noreply.github.com>
2025-03-11 20:27:13 +0900

    add SVG logos and update header component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,9 @@
-import { ModeToggle } from "@/app/components/mode-toggle";
+import { ModeToggle } from "@/app/_components/mode-toggle";
 
 
 export const Header = () => {
   return (
-    <div className="fixed flex justify-between px-8 w-screen h-16 items-center drop-shadow-2xl shadow-md">
+    <div className="fixed flex justify-between px-8 w-screen h-16 items-center drop-shadow-2xl border-b shadow-md">
       <h1 className="font-bold text-2xl">uniu.moe</h1>
       <div className="flex gap-3">
         <ModeToggle />
