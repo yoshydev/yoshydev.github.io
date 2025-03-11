f697e5c66b684348894e3b7ad9174d25330be5c5
yoshy <24353896+yoshydev@users.noreply.github.com>
2025-03-12 00:18:45 +0900

    fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,28 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "uniu.moe",
   description: "nya~n",
+  openGraph: {
+    title: "uniu.moe",
+    description: "nya~n",
+    images: [
+      {
+        url: "/images/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "uniu.moe",
+      },
+    ],
+    locale: "ja_JP",
+    type: "website",
+    url: "https://uniu.moe",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "uniu.moe",
+    description: "nya~n",
+    images: [ "/images/opengraph-image.png" ], // または別のOGP用画像パス
+    creator: "@yuniuiui",
+  },
 };
 
 export default function RootLayout({
