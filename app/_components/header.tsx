34b5c7a449cc9b33147445be050bc2db1157878f
yoshy <24353896+yoshydev@users.noreply.github.com>
2025-03-16 21:18:04 +0900

    update logo image in header component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
       <div
         className="fixed flex justify-between px-8 w-screen h-16 items-center drop-shadow-2xl border-b shadow-md z-50 bg-slate-50 dark:bg-slate-900">
         <div className="flex items-center gap-2">
-          <Image src="/images/logo.png" alt="logo" width="64" height="64" />
+          <Image src="/images/icon.png" alt="logo" width="48" height="48" />
           <h1 className="font-bold text-2xl">uniu.moe</h1>
         </div>
         <div className="flex gap-3">
