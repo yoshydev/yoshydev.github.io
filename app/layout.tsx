5515455d3ba9d18f4437f171c970dbdd4604d3db
yoshy <24353896+yoshydev@users.noreply.github.com>
2025-03-16 18:45:38 +0900

    update language attribute in layout to Japanese

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
     <body
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
