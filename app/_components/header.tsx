a8e0df4f54c351a5b28cdb9fbbab14028bc79357
yoshy <24353896+yoshydev@users.noreply.github.com>
2025-03-16 19:29:12 +0900

    add logo image to header component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,12 +1,18 @@
 import { ModeToggle } from "@/app/_components/mode-toggle";
+import Image from "next/image";
 
 export const Header = () => {
   return (
-    <div
-      className="fixed flex justify-between px-8 w-screen h-16 items-center drop-shadow-2xl border-b shadow-md z-50 bg-slate-50 dark:bg-slate-900">
-      <h1 className="font-bold text-2xl">uniu.moe</h1>
-      <div className="flex gap-3">
-        <ModeToggle />
+    <div>
+      <div
+        className="fixed flex justify-between px-8 w-screen h-16 items-center drop-shadow-2xl border-b shadow-md z-50 bg-slate-50 dark:bg-slate-900">
+        <div className="flex items-center gap-2">
+          <Image src="/images/logo.png" alt="logo" width="64" height="64" />
+          <h1 className="font-bold text-2xl">uniu.moe</h1>
+        </div>
+        <div className="flex gap-3">
+          <ModeToggle />
+        </div>
       </div>
     </div>
   );
